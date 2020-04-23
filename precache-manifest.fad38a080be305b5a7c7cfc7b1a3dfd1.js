b94b3bc1c827d947802267916b7a8526a7a51620
Jack Antcio <anticoj@bc.edu>
2020-04-23 15:52:47 -0400

    Updates

---
+++ precache-manifest.fad38a080be305b5a7c7cfc7b1a3dfd1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdc47b311b4d79e7f31e21fc5facd6a",
+    "url": "/jibra/index.html"
+  },
+  {
+    "revision": "50dcc359fff5c8b20d79",
+    "url": "/jibra/static/css/main.f78bf270.chunk.css"
+  },
+  {
+    "revision": "92283970f7a96f7733ab",
+    "url": "/jibra/static/js/2.a755fc80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jibra/static/js/2.a755fc80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50dcc359fff5c8b20d79",
+    "url": "/jibra/static/js/main.13a4de5c.chunk.js"
+  },
+  {
+    "revision": "1d046741031329646415",
+    "url": "/jibra/static/js/runtime-main.f4d15cb1.js"
+  },
+  {
+    "revision": "2bcc8c84d20ad9c38334a92240214d42",
+    "url": "/jibra/static/media/bcdm.2bcc8c84.png"
+  },
+  {
+    "revision": "4d16278f28bd6967a7bb8c33dbe2d19b",
+    "url": "/jibra/static/media/cacc.4d16278f.png"
+  },
+  {
+    "revision": "6e862d88503d2febf0c77318737cea31",
+    "url": "/jibra/static/media/cc.6e862d88.png"
+  },
+  {
+    "revision": "d8f74582c844e2c5aa8afbf60b2b3b91",
+    "url": "/jibra/static/media/pm.d8f74582.png"
+  },
+  {
+    "revision": "182f2cd91759b4169f8d19a88a4e80d1",
+    "url": "/jibra/static/media/tpmi.182f2cd9.png"
+  }
+]);
