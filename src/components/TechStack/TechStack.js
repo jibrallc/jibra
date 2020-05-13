364daf3e3a4f3ac3a24626c95ff26e9b1cfeadde
Jack Antcio <anticoj@bc.edu>
2020-05-13 10:24:19 -0400

    Fixed typos

--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -7,7 +7,7 @@ class TechStack extends Component {
     return (
       <div className="techStack" id="techStack">
         <h1 className="techStackHeader">Our Tech Stacks</h1>
-        <p className="techStackText"> We use a variety of Tech Stacks. With Web, it's mostly React.js on the frontend with NoSQL on the backend. With Mobile, we work with Swift on the frontend and also NoSQL on the backend. We provide all of clients with DNS management and traffic analytics accounts. We love to learn new technologies and are always willing to work with something new.</p>
+        <p className="techStackText"> We use a variety of Tech Stacks. With Web, it's mostly React.js on the frontend with NoSQL on the backend. With Mobile, we work with Swift on the frontend and also NoSQL on the backend. We provide all of our clients with DNS management and google analytics accounts. We love to learn new technologies and are always willing to work with something new.</p>
       </div>
     );
   }
